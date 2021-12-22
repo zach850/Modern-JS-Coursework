50cb81b5580770b3b8244f18c7ba1e44b8ef522a
Zach Richter <zach.richter85@gmail.com>
2021-12-21 18:35:07 -0800

    first commit

---
+++ Section 2_ JS Fundamentals/js-sandbox/app.js
@@ -0,0 +1,30 @@
+// Log to Console
+// console.log('hello zach');
+// console.log(134);
+// console.log(true);
+// console.log([1,2,3,4]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
+
+// console.error('This is some error');
+// console.warn('This is a warning');
+// console.time('Hello');
+//     console.log('hello zach');
+//     console.log('hello zach');
+//     console.log('hello zach');
+// console.timeEnd('Hello');
+
+// // var, let, const
+
+// var name = 'John Doe';
+// console.log(name);
+
+// //Init VAR
+
+// var greeting;
+// console.log(greeting);
+// greeting= 'Hello';
+// console.log(greeting);
+
+// var can include letters, numbers, _, $
+
---
+++ Section 2_ JS Fundamentals/js-sandbox/dataTypes.js
@@ -0,0 +1,29 @@
+// Primitive Data Types 
+// String, Number, Boolean, Null, Undefined, Symbols 
+
+//String
+const name = 'John Doe';
+//Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Undefined 
+let test; 
+//Symbol 
+const sym = Symbol();
+
+
+// Reference types 
+// Arrays, object literals, functions, dates, anything else
+
+//array 
+cont hobbies = ['movies', 'music'];
+
+//object literal 
+const address = {
+    city = 'Boston',
+    state = 'MA'
+}
+const today = new Date();
+
+console.log();
